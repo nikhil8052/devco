a5e9b6bcb459b87d2f92f1af009ffb41991d7731
nikk <nikk@gmail.com>
2024-10-16 15:57:58 +0530

    new changtes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
 
 
 export default function Home() {
   return (
     <>
-    
+
 
       <h1> THis is my design man </h1>
     </>
