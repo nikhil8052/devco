429a964087c96ad9842bcda3ad677c3204ce8a52
nikk <nikk@gmail.com>
2024-10-16 16:03:24 +0530

    more changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
