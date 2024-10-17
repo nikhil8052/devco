14dad8b409334bc7fc4d713cd739e3b69a32d6e8
nikk <nikk@gmail.com>
2024-10-17 11:51:11 +0530

    New changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import HeroBanner from "./components/layout/HeroBanner"
+import HeroBanner from "./components/layout/HeroBanner";
+import Faq from "./components/faq/Faq";
+
 
 export default function Home() {
   return (
     <>
       <HeroBanner />
+      <Faq /> 
+      
     </>
 
 
