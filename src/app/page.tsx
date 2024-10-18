926911ed9f09fe9f2801d3d5675e8212075608a7
nikk <nikk@gmail.com>
2024-10-18 13:37:38 +0530

    New changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
-
 import HeroBanner from "./components/layout/HeroBanner";
 import Faq from "./components/faq/Faq";
+import BasicCard from "./components/cards/BasicCard";
+import { homeCard1 } from "./data/home.js";
 
 export default function Home() {
   return (
     <>
-
-
-    <div className="home_page bg-black relative text-customwhite items-center pt-20" style={{
+      <div
+        className="home_page bg-black relative text-customwhite items-center pt-20"
+        style={{
           backgroundImage: "url('/images/banner_bg.png')",
           backgroundRepeat: "no-repeat",
-        }}>
-      <HeroBanner />
-        <Faq /> 
-    </div>
-    
-
+        }}
+      >
+        <HeroBanner />
+        {homeCard1.map((cardData, index) => (
+          <BasicCard key={index} cardData={cardData} />
+        ))}
+        <Faq />
+      </div>
     </>
   );
 }
