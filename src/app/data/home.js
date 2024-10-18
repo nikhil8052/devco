9f1aa9e6803d5cb8c8fe98e6669abc64e8d2622d
nikk <nikk@gmail.com>
2024-10-18 18:41:13 +0530

    test

--- src/app/data/home.js
+++ src/app/data/home.js
@@ -1,7 +1,5 @@
 
 
-
-
 export const homeCard1 = [
     {
         title: "Services",
@@ -276,3 +274,25 @@ export const service3={
     serviceProps:homeCard3,
     backgroundImage:'/images/right_circle.svg'
 }
+
+
+
+// Client count section data 
+ export  const clientCountData1 = {
+    imageSrc: '/images/cc1.svg',
+    imageWidth: 180,
+    imageHeight: 74,
+    title: 'Website Development',
+    description: 'Our web development team builds dozens of websites monthly, integrating the right technology stack with cross platform applications for the best possible user experience to your web apps.',
+  };
+
+export const clientCountData2 = {
+    imageSrc: '/images/cc1.svg',
+    imageWidth: 180,
+    imageHeight: 74,
+    title: 'Website Development',
+    description: 'Our web development team builds dozens of websites monthly, integrating the right technology stack with cross platform applications for the best possible user experience to your web apps.',
+  };
+
+
+// End client count section 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,8 +21,6 @@ export default function Home() {
         <DesignSection title={service1.title} description={service1.description} servicesProps={service1.serviceProps} backgroundImage={service1.backgroundImage} sideImage={service1.sideImage} />
         <DesignSection title={service2.title} description={service2.description} servicesProps={service2.serviceProps} backgroundImage={service2.backgroundImage} sideImage={service2.sideImage} />
         <DesignSection title={service3.title} description={service3.description} servicesProps={service3.serviceProps} backgroundImage={service3.backgroundImage} sideImage={service3.sideImage} />
-
-
       <Faq /> 
 
 
