500d3bd2d34a4c9bc3066a7e26ed43c318b6a90c
nikk <nikk@gmail.com>
2024-10-23 16:33:03 +0530

    added dynamic routes for the Tech and Skills

---
+++ src/app/services/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>This is the service page. Will will get it completed as soon as it is possible for us. We are working hard over it and get it ready in some of the time so please wait until we get it done. </div>
+  )
+}
