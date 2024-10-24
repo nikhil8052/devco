0cad281757f57e25458d2bbab10e3e14e3e45c1a
nikk <nikk@gmail.com>
2024-10-24 19:03:45 +0530

    Fixed the processes file now

--- src/app/components/Services/Processess.js
+++ src/app/components/Services/Processess.js
@@ -5,7 +5,7 @@ export default function Benefits({title="", description="", processes=[]}) {
   return (
     <div className="benefits-section py-12">
       <div className="text-center mb-8">
-        <h2 className="text-2xl md:text-3xl font-semibold">{title}</h2>
+        <h2 className="text-2xl md:text-3xl font-semibold">{title+description}</h2>
       </div>
 
       <div className="benefits-container grid grid-cols-1 md:grid-cols-2 gap-6 p-6  mx-auto">
