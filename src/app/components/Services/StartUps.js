3d45958cb108ac81b9bb75f66c6819382d8ee94e
nikk <nikk@gmail.com>
2024-10-24 19:00:54 +0530

    Removed the conflict

--- src/app/components/Services/StartUps.js
+++ src/app/components/Services/StartUps.js
@@ -11,19 +11,12 @@ export default function StartUps({title="",description="" , images= [], imageHei
     <div className="startup_section pb-20">
       <div className="container mx-auto p-4">
         <div className="text-center mb-6">
-<<<<<<< HEAD
+
           <h2 className="text-[22px] text-customwhite font-semibold">
            {title}
           </h2>
           <p className="text-[24px] font-normal text-customwhite">
             {description}
-=======
-          <h2 className="lg:text-[22px] md:text-[18px] sm:text-[18px] text-customwhite font-semibold">
-            Web Development & Software Engineering
-          </h2>
-          <p className="lg:text-[24px] md:text-[18px] sm:text-[14px] font-normal text-customwhite">
-            From next-gen startups to established enterprises.
->>>>>>> Rajeev
           </p>
         </div>
 
