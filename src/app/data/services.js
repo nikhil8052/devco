5260a998afe887c812521c9aac4484087f65d433
sourav <you@example.com>
2024-11-25 19:35:41 +0530

    25/11/24

--- src/app/data/services.js
+++ src/app/data/services.js
@@ -614,4 +614,126 @@ export const skills = [
             ],
         },
     },
+    {
+        id: 6,
+        slug: "devops",
+        top_title: "DevOps Services",
+        top_description: "DevOps, a combination of “development” and “operations,” is a set of philosophies and processes meant to make it faster and more efficient to develop software. Unfortunately, implementing DevOps practices is a challenge for many existing businesses.<br> At DEV.co, we offer DevOps as a service, providing you the DevOps tools, operations teams and a logistical gateway that allows you to achieve your software development goals.",
+        startups: {
+            title: "Technology Strategy",
+            description: "We create robust and future-proof technology infrastructures tailored to your business goals.",
+            imageHeight: 100,
+            imageWidth: 100,
+            images: ["/images/php.svg", "/images/unity.svg", "/images/python.svg", "/images/reactjs1.svg", "/images/java1.svg", "/images/nodejs1.svg"],
+        },
+        benefits: {
+            title: "The Benefits of Hiring a Software Development Staffing Company",
+            description: "Hiring a software development staffing company yields multiple benefits:",
+            benefits: [
+                {
+                    title: "Save Time",
+                    description: "First, a staffing agency will save you time. Rather than investing countless hours into a recruiting strategy that may or may not pay off, you can develop a recruiting strategy with a staffing firm and let them take care of most of the grunt work. Instead, you can focus your efforts where they’re needed most.",
+                    imageSrc: "/images/rotation_mug.svg",
+                    imageHeight: 27,
+                    imageWidth: 30,
+                },
+                {
+                    title: "Save Money",
+                    description: "It may seem counterintuitive, since you’ll typically need to pay for staffing services, but a software development staffing company can help you save money. You’ll face less overhead, since you’ll be able to thrive with fewer full-time, onsite software engineers, and you can reduce overall labor costs dramatically.",
+                    imageSrc: "/images/save_money.svg",
+                    imageHeight: 30,
+                    imageWidth: 28,
+                },
+                {
+                    title: "Get access to top talent",
+                    description: "At the same time, a software development staffing company helps you get access to some of the best talent in the industry. Whatever skill sets you’re looking for, you’ll be able to find worthy candidates who give you what you need. Depending on the complexity of the project you’re working on, this may be the best way to find the talent you require.",
+                    imageSrc: "/images/top_talent.svg",
+                    imageHeight: 30,
+                    imageWidth: 40,
+                },
+                {
+                    title: "Cover all your skilled needs",
+                    description: "A software development staffing company will have access to people with complex skills and experience in fields like ML, IoT, the blockchain, AR, VR, and more. You’ll also get access to candidates with specific experience in various industries, like healthcare, manufacturing, and real estate.",
+                    imageSrc: "/images/coding.svg",
+                    imageHeight: 30,
+                    imageWidth: 35,
+                },
+            ],
+        },
+        chooseServices: {
+            title: "How to Choose a Software Development Staffing Company",
+            description: `Of course, not all software development staffing companies are created equal. When searching for a potential staffing partner, make sure you consider:`,
+            card_data: [
+                {
+                    title: "Experience",
+                    description: "Valuable IT and software development staffing companies have plenty of experience. They understand the core needs of most of their clientele and can anticipate the usual barriers to success.",
+                    imgSrc: "/images/card_1.svg",
+                },
+                {
+                    title: "Speed",
+                    description:
+                        "Even if your project isn’t time-sensitive, you’ll still want a partner who can get you the staff members you need as quickly as possible. That way, you can get results faster—and potentially start generating revenue faster.",
+                    imgSrc: "/images/card_real.svg",
+                },
+                {
+                    title: "Access",
+                    description: "What kinds of talent is this staffing agency able to secure? Do they have access to people in a variety of different niches? What are your needs?",
+                    imgSrc: "/images/accessimg.svg",
+                },
+                {
+                    title: "Communication",
+                    description: "A good staffing partner will collaborate with you, and collaboration demands effective communication. Is it easy to talk with this partner and discuss your vision?",
+                    imgSrc: "/images/comminuctn.svg",
+                },
+            ],
+        },
+        processes: {
+            title: "Our Development <br> Process",
+            description: "",
+            processes: [
+                {
+                    title: "Understanding",
+                    description: `Background details<br>
+                                    Vision & values<br>
+                                    Target personas<br>
+                                    Stakeholder insights`,
+                    imageSrc: "/images/num-1.svg",
+                    imageHeight: 100,
+                    imageWidth: 100,
+                },
+                {
+                    title: "Research",
+                    description:`Customer research<br>Competitive analysis<br>Expert audits & insights<br>Current market positioning`,
+                    imageSrc: "/images/num-2.svg",
+                    imageHeight: 100,
+                    imageWidth: 100,
+                },
+                {
+                    title: "Strategy",
+                    description: `Story & personality<br>Market opportunities<br>Positioning & messaging<br>Creative brief development`,
+                    imageSrc: "/images/num-3.svg",
+                    imageHeight: 100,
+                    imageWidth: 100,
+                },
+                {
+                    title: "Creative",
+                    description: `Naming<br>
+                                Logo development<br>
+                                Graphics standards<br>
+                                Verify function<br>
+                                Messaging framework`,
+                    imageSrc: "/images/num-4.svg",
+                    imageHeight: 100,
+                    imageWidth: 100,
+                },
+                {
+                    title: "Deliver",
+                    description: `Brand book & styleguide<br>Print integration<br>Online & mobile<br>Further brand extension`,
+                    imageSrc: "/images/num-5.svg",
+                    imageHeight: 100,
+                    imageWidth: 100,
+                },
+            ],
+        },
+    },
 ];
