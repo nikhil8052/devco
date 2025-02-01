86bd8bdbe0a867aee345e585e1fe4ebd8823b6a5
Your Name <you@example.com>
2025-02-01 14:42:28 +0530

    site map fixed

--- src/app/data/sitemap.js
+++ src/app/data/sitemap.js
@@ -1,4 +1,4 @@
-export const metadata = {
+export default const metadata = {
     title: "Map | Sitemap | DEV.co | Custom Software Development Company",
     description: "Complete DEV.co sitemap. Find anything related to our software development web development services via our sitemap!",
     image: "/images/Map-Custom-Software-Development-Company.jpg",
