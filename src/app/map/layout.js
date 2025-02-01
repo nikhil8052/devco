f9d1273d4cbc8c37b02fff10f9e242e7d3256326
Your Name <you@example.com>
2025-02-01 15:04:03 +0530

    Removed the code for the site map

--- src/app/map/layout.js
+++ src/app/map/layout.js
@@ -25,23 +25,23 @@
 
 import metadata from "@/app/data/sitemap";
 
-export const metadata1 = {
-  title: metadata.title,
-  description: metadata.description,
-  openGraph: {
-    title: metadata.title,
-    description: metadata.description,
-    type: "website",
-    images: [
-      {
-        url: metadata.image,
-        width: 1200,
-        height: 630,
-        alt: "Software Development Company",
-      },
-    ],
-  },
-};
+// export const metadata1 = {
+//   title: metadata.title,
+//   description: metadata.description,
+//   openGraph: {
+//     title: metadata.title,
+//     description: metadata.description,
+//     type: "website",
+//     images: [
+//       {
+//         url: metadata.image,
+//         width: 1200,
+//         height: 630,
+//         alt: "Software Development Company",
+//       },
+//     ],
+//   },
+// };
 
 export default function Layout({ children }) {
   return <>{children}</>;
