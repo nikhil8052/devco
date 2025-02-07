670c4b2d85838015eba6b10786d94c3a1765b503
rajeev@gmail.com <rajeev@gmail.com>
2025-02-07 19:47:33 +0530

    career page

--- src/app/components/siteforms/Careerpage.js
+++ src/app/components/siteforms/Careerpage.js
@@ -42,7 +42,7 @@ const Careerpage = () => {
       return;
     }
 
-    const apiUrl = 'https://devco1.wpenginepowered.com/wp-json/custom/v1/send-mail?username=devdotco&password=MnFI 4eZL xMDN SWF0 WZa6 AmiX';
+    const apiUrl = 'https://devco1.wpenginepowered.com/wp-json/custom/v1/send-career?username=devdotco&password=MnFI 4eZL xMDN SWF0 WZa6 AmiX';
     const payload = { ...data, resume };
 
     try {
