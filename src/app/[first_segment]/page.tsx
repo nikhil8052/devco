d15217085b6c6f59d84fe16e146db63c08f55d99
Nikhil <nikhil@gmail.com>
2025-02-07 11:59:42 +0530

    Fixed the error

--- src/app/[first_segment]/page.tsx
+++ src/app/[first_segment]/page.tsx
@@ -12,7 +12,7 @@ import Service from "@/app/ourservices/Service";
 import Technology from "@/app/technology/Technology";
 import Locations from "@/app/locations/Locations";
 import UserLayout from "../user_layout/UserLayout";
-import BlogPage from "@/app/blog_test/blog_page_test/page";
+import BlogPage from "@/app/posts/Post";
 
 // Define types for data
 interface BaseData {
